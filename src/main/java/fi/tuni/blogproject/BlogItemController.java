47dc565e1cb3553307fae1fc71a4143f9efc79c5
Jimi Savola <savola.jimi@gmail.com>
2019-04-18 16:29:09 +0300

    Divide Controller into two separate RestControllers
    - Divide Controller into two separate Rest Controllers for better readability and easier future development
    - Change Controller into BlogItemController to handle all database changes for BlogItems
    - Add CommentController to handle all database changes for Comments

--- src/main/java/fi/tuni/blogproject/BlogItemController.java
+++ src/main/java/fi/tuni/blogproject/BlogItemController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Optional;
 
 @RestController
-public class Controller {
-
+public class BlogItemController {
     @Autowired
     BlogItemRepository blogItemRepository;
 
@@ -45,38 +42,5 @@ public class Controller {
     public Optional<BlogItem> getBlogItem(@PathVariable String blogId) {
         return blogItemRepository.findById(blogId);
 
-    }
-
-    @RequestMapping(value="/addComment", method= RequestMethod.POST)
-    @ResponseBody
-    public Comment addComment(@RequestBody Comment c) {
-        c.setLike(0);
-        c.setCommentDate(new Date());
-
-        commentRepository.save(c);
-
-        return c;
-    }
-
-    @GetMapping("getComments/{blogId}")
-    public Iterable<Comment> getComments(@PathVariable String blogId) {
-        ArrayList<Comment> comments = new ArrayList<>();
-
-        for (Comment c : commentRepository.findAll()) {
-            if (c.getBlogId().equals(blogId)) {
-                comments.add(c);
-            }
-
-        }
-        return comments;
-    }
-
-    @RequestMapping(value="/likeComment", method= RequestMethod.POST)
-    @ResponseBody
-    public Optional<Comment> likeComment(@RequestBody Comment c) {
-        c.setLike(1);
-        commentRepository.save(c);
-
-        return Optional.ofNullable(c);
     }
 }
