55c3c365e1c602b7722d1436254fe70144fead26
Jimi Savola <savola.jimi@gmail.com>
2019-04-18 16:45:54 +0300

    Delete unused Autowiring and imports from main class

--- src/main/java/fi/tuni/blogproject/BlogprojectApplication.java
+++ src/main/java/fi/tuni/blogproject/BlogprojectApplication.java
@@ -1,20 +1,11 @@
 package fi.tuni.blogproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.Date;
 
 @SpringBootApplication
 public class BlogprojectApplication implements CommandLineRunner {
-	@Autowired
-    BlogItemRepository blogItemRepository;
-
-	@Autowired
-    CommentRepository commentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogprojectApplication.class, args);
