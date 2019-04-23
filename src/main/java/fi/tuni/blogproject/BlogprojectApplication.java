1a0695d71e1b1a6ff98fca048d6ba48cea3ec5d7
Jimi Savola <savola.jimi@gmail.com>
2019-04-23 13:42:50 +0300

    Add creation of few sample blog posts, when server starts

--- src/main/java/fi/tuni/blogproject/BlogprojectApplication.java
+++ src/main/java/fi/tuni/blogproject/BlogprojectApplication.java
@@ -1,6 +1,7 @@
 package fi.tuni.blogproject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,6 +17,9 @@ import java.util.List;
  */
 @SpringBootApplication
 public class BlogprojectApplication implements CommandLineRunner {
+    @Autowired
+    BlogItemRepository blogItemRepository;
+
     /**
      * Main method.
      *
@@ -25,13 +30,16 @@ public class BlogprojectApplication implements CommandLineRunner {
 	}
 
     /**
-     * Prints all possible curl commands to console on application start.
+     * Prints all possible curl commands to console on application start, and makes
+     * few sample blog posts.
      *
      * @param args Command line arguments.
      * @throws IOException Exception for failed or interrupted I/O operations.
      */
 	@Override
 	public void run(String... args) throws IOException {
+	    createBlogPosts();
+
 	    for(CurlCommand cc : getCurlCommands()) {
 	        System.out.println("\nFunction: " + cc.getName());
 	        System.out.println("Command: " + cc.getCommand());
@@ -48,5 +56,16 @@ public class BlogprojectApplication implements CommandLineRunner {
 	    final String resourceFile = "curl.commands.json";
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resourceFile);
         return Arrays.asList(new ObjectMapper().readValue(inputStream, CurlCommand[].class));
+    }
+
+    /**
+     * Creates sample data to database.
+     */
+    public void createBlogPosts() {
+	    BlogItem b1 = new BlogItem(new Date(), "Tester", "Test", "Sample text");
+        BlogItem b2 = new BlogItem(new Date(), "Jester", "Test 2", "Such wow");
+
+        blogItemRepository.save(b1);
+        blogItemRepository.save(b2);
     }
 }
