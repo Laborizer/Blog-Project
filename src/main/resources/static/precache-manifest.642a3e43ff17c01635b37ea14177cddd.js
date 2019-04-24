f780d51a8cf0b152ebcbf6abec3e5d5ef170c889
Jimi Savola <savola.jimi@gmail.com>
2019-04-24 22:24:50 +0300

    Create static folder in backend resources directory

---
+++ src/main/resources/static/precache-manifest.642a3e43ff17c01635b37ea14177cddd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22caa8394171c7e777f1",
+    "url": "/static/js/main.4892bc2d.chunk.js"
+  },
+  {
+    "revision": "62dc3d736c1fe54e8407",
+    "url": "/static/js/2.8b599f44.chunk.js"
+  },
+  {
+    "revision": "22caa8394171c7e777f1",
+    "url": "/static/css/main.798aa6cb.chunk.css"
+  },
+  {
+    "revision": "c4816b42b87f47c1111d0290edf559cc",
+    "url": "/index.html"
+  }
+];
